1496c9dada9e818b5df9615b31a40a19b5052033
Selim <selimdogan303@gmail.com>
2023-09-03 16:40:16 +0300

    closed #4

--- Cantin.Web/wwwroot/js/saleAdd.js
+++ Cantin.Web/wwwroot/js/saleAdd.js
@@ -1,44 +1,80 @@
 ﻿//let table = new DataTable("#ProductsTable");
-
-$(document).ready(function () {
-    var table = $('#ProductsTable').DataTable();
-    table.on('click', 'button.reduce', function (e) {
-        let tr = $(this).closest('tr');
-        let row = table.row(tr);
-        console.log(row);
-    });
-    class ProductLine {
-        constructor(product) {
-            this.Product = product;
-            this.Quantity = 1;
-            this.SubTotal = this.calculateSubTotal();
-        }
-
-        increase() {
-            //Test
-            this.Quantity = this.Quantity + 1;
-        }
-
-        calculateSubTotal() {
-            this.SubTotal = parseFloat(this.Product.salePrice) * this.Quantity;
-            return this.SubTotal;
-        }
+class ProductLines {
+    constructor() {
+        this.ProductLines = new Array();
+        this.SoldTotal = 0;
     }
-    class ProductLines {
-        constructor() {
-            this.ProductLines = new Array();
-            this.SoldTotal = 0;
+    reDrawTable(table) {
+        table.clear().draw();
+        if (this.ProductLines.length > 0) {
+            this.ProductLines.forEach(function (line) {
+                let product = line.Product;
+                let reduceButton = `<button class='btn btn-danger reduce'>
+                        Azalt
+                        </button>
+                    `;
+                let deleteLineButton = `<button class='btn btn-danger deleteLine'>
+                        Sil
+                        </button>
+                    `;
+                table.row.add([product.name, product.barcode, product.salePrice, line.Quantity, line.SubTotal, reduceButton, deleteLineButton]).draw();
+            });
         }
         
     }
-    var calculateTotal = function(lines) {
+
+}
+class ProductLine {
+    constructor(product) {
+        this.Product = product;
+        this.Quantity = 1;
+        this.SubTotal = this.calculateSubTotal();
+    }
+
+    increase() {
+        //Test
+        this.Quantity = this.Quantity + 1;
+    }
+
+    calculateSubTotal() {
+        this.SubTotal = parseFloat(this.Product.salePrice) * this.Quantity;
+        return this.SubTotal;
+    }
+}
+$(document).ready(function () {
+    var table = $('#ProductsTable').DataTable();
+    let lines = new ProductLines();
+    table.on('click', 'button.reduce', function (e) {
+        let tr = $(this).closest('tr');
+        let row = table.row(tr);
+        let line = lines.ProductLines.filter(line => line.Product.name === row.data()[0])[0]
+        line.Quantity -= 1;
+        if (line.Quantity === 0) {
+            index = lines.ProductLines.indexOf(line);
+            lines.ProductLines.splice(index, 1);
+        }
+        else {
+            line.calculateSubTotal();
+        }
+        lines.reDrawTable(table);
+        let total = calculateTotal(lines.ProductLines);
+        document.getElementById('total').innerText = "Toplam: " + total.toString();
+    });
+    table.on('click', 'button.deleteLine', function (e) {
+        let tr = $(this).closest('tr');
+        let row = table.row(tr);
+        let index = lines.ProductLines.indexOf(lines.ProductLines.filter(line => line.Product.name === row.data()[0])[0]);
+        lines.ProductLines.splice(index, 1);
+        lines.reDrawTable(table);
+    });
+    var calculateTotal = function (lines) {
         var total = 0;
         lines.forEach(function (line) {
             total += parseFloat(line.SubTotal);
         });
         return total;
     }
-    var lines = new ProductLines();
+
     $('#myButton').click(function () {
         var barcode = $("#barkodInput").val();
         $.ajax({
@@ -46,8 +82,6 @@ $(document).ready(function () {
             type: 'GET',
             success: function (data) {
                 var line;
-
-
                 if (lines.ProductLines.length > 0) {
                     console.log(data.id);
                     line = lines.ProductLines.filter(x => x.Product.id === data.id);
@@ -55,7 +89,6 @@ $(document).ready(function () {
                         console.log(myline);
                     });
                 }
-
                 if (!line || line.length === 0) {
                     line = new ProductLine(data);
                     line.ProductId = data.id;
@@ -66,23 +99,11 @@ $(document).ready(function () {
                     line.calculateSubTotal(); // Recalculate the SubTotal
                 }
 
-                table.clear();
-                let row = 0;
-                lines.ProductLines.forEach(function (line) {
-                    row += 1;
-                    let product = line.Product;
-                    let reduceButton = `<button class='text-danger reduce'>
-                        -
-                        </button>
-                    `;
-                    table.row.add([product.name, product.barcode, product.salePrice, line.Quantity, line.SubTotal,reduceButton]).draw();
-                });
+                lines.reDrawTable(table);
                 let total = calculateTotal(lines.ProductLines);
                 document.getElementById('total').innerText = "Toplam: " + total.toString();
-
             }
         });
-
     });
     $('#addButton').click(function () {
         lines.SoldTotal = calculateTotal(lines.ProductLines);
